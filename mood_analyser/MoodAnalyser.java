53e02323c106b17e551069355fee8686cbeb8135
vasundhrajamwal <vasundhra.jamwal@gmail.com>
2023-06-06 17:19:28 +0530

    Code to Handle Exception with Invalid Mood

--- mood_analyser/MoodAnalyser.java
+++ mood_analyser/MoodAnalyser.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.mood_analyser;
-
 public class MoodAnalyser {
     String message;
 
-    public String analyseMood(String message) {
-        if (message.contains("sad"))
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood(){
+
+        if(this.message.contains("sad"))
             return "SAD";
         else
             return "HAPPY";
--- mood_analyser/MoodAnalyserTest.java
+++ mood_analyser/MoodAnalyserTest.java
@@ -5,14 +5,21 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     @Test
     public void givenMessage_whenSad_shouldReturnSadMood(){
-        MoodAnalyser obj = new MoodAnalyser();
-        String res = obj.analyseMood("I am in sad mood");
+        MoodAnalyser obj = new MoodAnalyser("I am in sad mood");
+        String res = obj.analyseMood();
         Assert.assertEquals("SAD", res);
     }
     @Test
     public void givenMessage_whenAny_shouldReturnHappyMood(){
-        MoodAnalyser obj = new MoodAnalyser();
-        String res = obj.analyseMood("I am in happy Mood");
+        MoodAnalyser obj = new MoodAnalyser("I am in happy Mood");
+        String res = obj.analyseMood();
+        Assert.assertEquals("HAPPY", res);
+    }
+
+    @Test
+    public void givenMessage_whenNull_shouldReturnHappy(){
+        MoodAnalyser obj = new MoodAnalyser("NULL");
+        String res = obj.analyseMood();
         Assert.assertEquals("HAPPY", res);
     }
 }
